452a6c4dd11de786700379a873fce88ceb326390
rod3ricksan <randy.cardenas75@gmail.com>
2020-11-04 02:47:18 -0300

    Agrege inteligencia a seguridad y unas layouts que nos faltaban

--- app/src/main/java/com/example/bancobpm_act/MainActivity.java
+++ app/src/main/java/com/example/bancobpm_act/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.bancobpm_act;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edit1, edit2;
+    ProgressDialog progressDialog;
+    Button btIniciar;
+
 
 
 
@@ -21,31 +23,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btIniciar = findViewById(R.id.btnIniciar);
         edit1 = (EditText) findViewById(R.id.et1);
         edit2 = (EditText) findViewById(R.id.et2);
+        btIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = edit1.getText().toString();
+                String contra = edit2.getText().toString();
+
+                if (usuario.equalsIgnoreCase("android") && contra.equals("123"))
+                {
+                    Toast.makeText(getBaseContext(), "Sesión Iniciada Correctamente", Toast.LENGTH_SHORT).show();
+
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.show();
+                    progressDialog.setContentView(R.layout.progress_dialog);
+                     progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                    Intent i = new Intent(getBaseContext(), Home_act.class);
+                    startActivity(i);
+                }
+                else
+                    {
+                    Toast.makeText(getBaseContext(), "Usuario e/o Contraseña Incorrecta", Toast.LENGTH_SHORT).show();
+                    }
+             }
+        });
 
     }
-    public void Ingresar(View v) {
-
-        String usuario = edit1.getText().toString();
-        String contra = edit2.getText().toString();
-
-        switch (v.getId()) {
-            case R.id.button:
-                if (usuario.equalsIgnoreCase("android") && contra.equals("123")) {
-                    Toast.makeText(this, "Sesión Iniciada Correctamente", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(this, Home_act.class);
-                    startActivity(i);
-                } else {
-                    Toast.makeText(this, "Usuario e/o Contraseña Incorrecta", Toast.LENGTH_SHORT).show();
-                }
-
-                break;
-
-            default:
-                break;
-        }
-
-
+    @Override
+    public void onBackPressed()
+    {
+        progressDialog.dismiss();
     }
 }
